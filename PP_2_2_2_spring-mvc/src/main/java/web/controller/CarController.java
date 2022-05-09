0872c1137d04bd2ab6f09a51c75f43ad1bbeaf82
suwakoryukyu <hisoutensoku@suwako.ryukyu>
2022-05-09 22:05:48 +0500

    Changed capital letter in the name of directory

--- PP_2_2_2_spring-mvc/src/main/java/web/controller/CarController.java
+++ PP_2_2_2_spring-mvc/src/main/java/web/controller/CarController.java
@@ -3,15 +3,9 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.Service.CarService;
-import web.Service.CarServiceImpl;
-import web.model.Car;
-
-import java.util.ArrayList;
-import java.util.List;
+import web.service.CarService;
+import web.service.CarServiceImpl;
 
 @Controller
 public class CarController {
