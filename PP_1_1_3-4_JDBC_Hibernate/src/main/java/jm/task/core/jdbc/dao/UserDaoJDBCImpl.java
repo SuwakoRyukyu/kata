7abb1a01b3d04f1d0281b5165b472527e894f089
suwakoryukyu <hisoutensoku@suwako.ryukyu>
2022-06-03 03:55:57 +0500

    Minor fixes

--- PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,16 +2,20 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class UserDaoJDBCImpl implements UserDao {
 
-
-    private final Connection connection = Util.getConnection();
     public void createUsersTable() {
-        try (Statement st = connection.createStatement()) {
+        try (Connection connection = Util.getConnection();
+             Statement st = connection.createStatement()) {
             st.executeUpdate(
                     "CREATE TABLE IF NOT EXISTS Users(" +
                             "id INT NOT NULL AUTO_INCREMENT," +
@@ -19,70 +23,57 @@ public class UserDaoJDBCImpl implements UserDao {
                             "lastName VARCHAR (45) NOT NULL," +
                             "age INT NOT NULL," +
                             "PRIMARY KEY (id))");
-            connection.commit();
             System.out.println("The table Users was created");
         } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ignored) {
-            }
             e.printStackTrace();
         }
     }
 
     public void dropUsersTable() {
-        try (Statement st = connection.createStatement()) {
+        try (Connection connection = Util.getConnection();
+             Statement st = connection.createStatement()) {
             st.executeUpdate("DROP TABLE IF EXISTS Users");
-            connection.commit();
             System.out.println("The table Users was deleted");
         } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ignored) {
-            }
             e.printStackTrace();
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (PreparedStatement ps = connection.prepareStatement(
+        try (Connection connection = Util.getConnection();
+             PreparedStatement ps = connection.prepareStatement(
                      "INSERT INTO Users (name, lastName, age) " +
                              "VALUES (?, ?, ?)")) {
             ps.setNString(1, name);
             ps.setNString(2, lastName);
             ps.setInt(3, age);
             ps.executeQuery();
-            connection.commit();
             System.out.printf("Saved user %s, %s, %d\n", name, lastName, age);
         } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ignored) {
-            }
             e.printStackTrace();
         }
     }
 
     public void removeUserById(long id) {
-        try (PreparedStatement ps = connection.prepareStatement(
+        try (Connection connection = Util.getConnection();
+             PreparedStatement ps = connection.prepareStatement(
                      "DELETE FROM Users WHERE id = (?)")) {
             ps.setLong(1, id);
             ps.executeQuery();
-            connection.commit();
             System.out.println("User with id " + id + " was removed");
         } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ignored) {
-            }
             e.printStackTrace();
         }
     }
 
     public List<User> getAllUsers() {
         ArrayList<User> userList = new ArrayList<>();
-        try (Statement stat = connection.createStatement();
-            ResultSet rs = stat.executeQuery("SELECT * FROM Users")) {
+
+
+
+        try (Connection connection = Util.getConnection();
+             Statement stat = connection.createStatement();
+             ResultSet rs = stat.executeQuery("SELECT * FROM Users")) {
             while (rs.next()) {
                 User myUser = new User();
                 {
@@ -102,15 +93,11 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try (Statement st = connection.createStatement()) {
+        try (Connection connection = Util.getConnection();
+             Statement st = connection.createStatement()) {
              st.executeUpdate("TRUNCATE Users");
-             connection.commit();
             System.out.println("Table Users was cleared");
         } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ignored) {
-            }
             e.printStackTrace();
         }
     }
--- PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/util/Util.java
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,25 +1,26 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 // The reason I'm using Mariadb is that the default implementation of MySQL on my Linux distro (Arch Linux) is Mariadb, and there's no actual way to change it to MySQL as far as I can see.
 public class Util {
-
     private Util() {}
-
     private static Connection conn;
 
     // Singleton initialization of class Util
     public static Connection getConnection() {
-        if (conn == null) {
-            try {
+        try {
+            if (conn == null || conn.isClosed()) {
                 String url = "jdbc:mariadb://localhost:3306/mydb";
                 String user = "root";
                 String password = "suwako";
                 conn = DriverManager.getConnection(url, user, password);
-            } catch (SQLException e) {
-                e.printStackTrace();
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return conn;
     }
